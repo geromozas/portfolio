b9673c5946ec23b92843932e3d19d9a746def3fa
Geronimo Mozas <mozasgeronimo@gmail.com>
2024-11-08 21:19:52 -0300

    correcion de responsive

--- src/components/pages/skills/Skills.jsx
+++ src/components/pages/skills/Skills.jsx
@@ -38,50 +38,51 @@ const Skills = () => {
             className="skill"
           />
         </div>
-      </div>
-      <h3 className="titleSectionSkill">Backend</h3>
-      <div className="boxBackend">
-        <img
-          src="https://res.cloudinary.com/ddmvo0ert/image/upload/v1730213726/portfolio/nodejs_wxhmm7.png"
-          alt="node"
-          className="skill"
-        />
-        <img
-          src="https://res.cloudinary.com/ddmvo0ert/image/upload/v1730213725/portfolio/express-js_e4nqqf.png"
-          alt="express"
-          className="skill"
-        />
-      </div>
-      <h3 className="titleSectionSkill">Base de Datos</h3>
-      <div className="boxDataBase">
-        <img
-          src="https://res.cloudinary.com/ddmvo0ert/image/upload/v1730213725/portfolio/mongodb_vwzcp0.png"
-          alt="mongoDB"
-          className="skill"
-        />
-        <img
-          src="https://res.cloudinary.com/ddmvo0ert/image/upload/v1730213724/portfolio/firebase_vkpy6l.png"
-          alt="firebase"
-          className="skill"
-        />
-      </div>
-      <h3 className="titleSectionSkill">Herramientas y Otros</h3>
-      <div className="boxTolls">
-        <img
-          src="https://res.cloudinary.com/ddmvo0ert/image/upload/v1730213725/portfolio/git_baum67.png"
-          alt="git"
-          className="skill"
-        />
-        <img
-          src="https://res.cloudinary.com/ddmvo0ert/image/upload/v1730213725/portfolio/github_z8yinv.png"
-          alt="gitHub"
-          className="skill"
-        />
-        <img
-          src="https://res.cloudinary.com/ddmvo0ert/image/upload/v1730213725/portfolio/postman_x2vrjj.png"
-          alt="postman"
-          className="skill"
-        />
+
+        <h3 className="titleSectionSkill">Backend</h3>
+        <div className="boxBackend">
+          <img
+            src="https://res.cloudinary.com/ddmvo0ert/image/upload/v1730213726/portfolio/nodejs_wxhmm7.png"
+            alt="node"
+            className="skill"
+          />
+          <img
+            src="https://res.cloudinary.com/ddmvo0ert/image/upload/v1730213725/portfolio/express-js_e4nqqf.png"
+            alt="express"
+            className="skill"
+          />
+        </div>
+        <h3 className="titleSectionSkill">Base de Datos</h3>
+        <div className="boxDataBase">
+          <img
+            src="https://res.cloudinary.com/ddmvo0ert/image/upload/v1730213725/portfolio/mongodb_vwzcp0.png"
+            alt="mongoDB"
+            className="skill"
+          />
+          <img
+            src="https://res.cloudinary.com/ddmvo0ert/image/upload/v1730213724/portfolio/firebase_vkpy6l.png"
+            alt="firebase"
+            className="skill"
+          />
+        </div>
+        <h3 className="titleSectionSkill">Herramientas y Otros</h3>
+        <div className="boxTolls">
+          <img
+            src="https://res.cloudinary.com/ddmvo0ert/image/upload/v1730213725/portfolio/git_baum67.png"
+            alt="git"
+            className="skill"
+          />
+          <img
+            src="https://res.cloudinary.com/ddmvo0ert/image/upload/v1730213725/portfolio/github_z8yinv.png"
+            alt="gitHub"
+            className="skill"
+          />
+          <img
+            src="https://res.cloudinary.com/ddmvo0ert/image/upload/v1730213725/portfolio/postman_x2vrjj.png"
+            alt="postman"
+            className="skill"
+          />
+        </div>
       </div>
     </>
   );
